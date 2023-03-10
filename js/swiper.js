dfbc7f172c4944ed0a9c768d92a72f97e6b70735
SparkyPX <pierrelucproulx@gmail.com>
2023-03-10 13:00:57 -0500

    Meh

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    grabCursor: true,
+    centeredSlides: true,
+    freeMode: true,
+    effect: 'slider',
+    slidesPerView: 3,
+    spaceBetween: 30,
+  });
+
