92634a127c5bcb4eb756f48218facc8954449cd6
tonie2023 <tony_19m@hotmail.ca>
2023-03-15 17:25:57 -0400

    Sauvegarde partie finale landing page
    Il reste des correctifs à faire.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 function myFunction(x) {
   x.classList.toggle("fa-thumbs-down");
 }
+
+const swiper = new Swiper(".swiper-screenshots", {
+  direction: "horizontal",
+  effect: "slide",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 25,
+  centeredSlides: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
