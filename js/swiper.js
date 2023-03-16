bcaa5148890562505f597442d1f5f45f4dfe45d1
SparkyPX <pierrelucproulx@gmail.com>
2023-03-16 15:28:57 +0000

    Working on it

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,18 @@
 const swiper = new Swiper('.mySwiper', {
-    direction: 'horizontal',
-    grabCursor: true,
-    centeredSlides: true,
-    freeMode: true,
-    effect: 'slider',
-    slidesPerView: 3,
-    spaceBetween: 30,
-  });
-
+  direction: "horizontal",
+  effect: "slide",
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 3,
+  centeredSlides: false,
+  breakpoints: {
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 5,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
